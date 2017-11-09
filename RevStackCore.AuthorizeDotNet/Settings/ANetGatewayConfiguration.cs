6846d77dd2e77a1e28da1c40b965c0aa3082c215
Scott <sfrancis@misinteractive.com>
2017-11-09 18:21:59 -0500

    Added Settings GatewayConfiguration class

---
+++ RevStackCore.AuthorizeDotNet/Settings/ANetGatewayConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RevStackCore.AuthorizeDotNet
+{
+    public class ANetGatewayConfiguration
+    {
+        public string ApiLogin { get; set; }
+        public string ApiKey { get; set; }
+        public ANetProcessorModeType ApiMode { get; set; }
+    }
+}
